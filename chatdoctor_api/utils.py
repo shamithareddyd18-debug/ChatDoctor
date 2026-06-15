32bde164dcce05e05ba78f6923c2aedb62455605
shamithareddyd18-debug <shamithareddyd18@gmail.com>
2026-06-15 17:22:08 +0530

    Update utils.py

--- chatdoctor_api/utils.py
+++ chatdoctor_api/utils.py
@@ -1,3 +1,4 @@
+import json
 import datetime
 
 def get_timestamp():
@@ -5,3 +6,17 @@ def get_timestamp():
     return str(
         datetime.datetime.now()
     )
+
+
+def save_json(data, path):
+
+    with open(path, "w") as f:
+
+        json.dump(data, f, indent=4)
+
+
+def load_json(path):
+
+    with open(path, "r") as f:
+
+        return json.load(f)
