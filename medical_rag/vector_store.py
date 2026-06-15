47fce643ffd189fa81db17e7a2e833cad9e50a8f
shamithareddyd18-debug <shamithareddyd18@gmail.com>
2026-06-15 17:02:42 +0530

    Update vector_store.py

--- medical_rag/vector_store.py
+++ medical_rag/vector_store.py
@@ -1,1 +1,13 @@
+VECTOR_DB = []
 
+def add_document(
+    embedding,
+    text
+):
+
+    VECTOR_DB.append(
+        {
+            "embedding": embedding,
+            "text": text
+        }
+    )
